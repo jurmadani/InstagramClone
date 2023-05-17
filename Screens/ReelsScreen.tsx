610180cdaaaab2be9a2e06ebdf5da679ffa12b78
Daniel Jurma <d_jurma@yahoo.com>
2023-05-17 20:13:06 +0300

    Small UI Update to Reels Screen

--- Screens/ReelsScreen.tsx
+++ Screens/ReelsScreen.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, SafeAreaView, Text } from "react-native";
+import React from "react";
+import { windowHeight } from "../Constants/Dimensions";
 
 const ReelsScreen = () => {
   return (
-    <View>
-      <Text>ReelsScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1, justifyContent: "flex-start" }}>
+      <Image source={require("../assets/Images/no-signal-tv.gif")} />
+      <Text style={{ textAlign: "center", fontWeight: "bold", fontSize: 25, marginTop:20,}}>
+        No reels at the moment
+      </Text>
+    </SafeAreaView>
+  );
+};
 
-export default ReelsScreen
+export default ReelsScreen;
