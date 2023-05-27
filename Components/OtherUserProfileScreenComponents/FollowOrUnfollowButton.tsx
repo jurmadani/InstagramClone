a2140c368dc4b5f1f46f5eff3482321a4c4d5743
Daniel Jurma <d_jurma@yahoo.com>
2023-05-27 12:11:52 +0300

    Follow/Unfollow Functionality added: Updates redux global state & firestore db

--- Components/OtherUserProfileScreenComponents/FollowOrUnfollowButton.tsx
+++ Components/OtherUserProfileScreenComponents/FollowOrUnfollowButton.tsx
@@ -5,16 +5,174 @@ import { windowWidth } from "../../Constants/Dimensions";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StackParams } from "../../Navigator/StackNavigator";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { firebase } from "../../firebase";
+import { AnyAction, Dispatch } from "@reduxjs/toolkit";
+import { UserSlice } from "../../Redux/User";
+import { OtherUserSlice } from "../../Redux/OtherUser";
 
-const onPressHandler = () => {};
+const onPressHandler = async (
+  doesCurrentUserLoggedinFollowOtherUser: boolean,
+  setCurrentUserLoggedinFollowOtherUser: React.Dispatch<
+    React.SetStateAction<boolean>
+  >,
+  currentUserLoggedIn: any,
+  otherUser: any,
+  dispatch: Dispatch<AnyAction>
+) => {
+  if (!doesCurrentUserLoggedinFollowOtherUser) {
+    //update firestore and update redux global state
+    //update current user logged in following array
+    const followingTempArray = [...currentUserLoggedIn.following]; // Make a copy of the array
+    followingTempArray.push(otherUser.username);
+
+    await firebase
+      .firestore()
+      .collection("Users")
+      .doc(currentUserLoggedIn.username)
+      .update({
+        following: followingTempArray,
+      })
+      .then(() => {
+        //update redux global state
+        dispatch(
+          UserSlice.actions.setUser({
+            description: currentUserLoggedIn.description,
+            email: currentUserLoggedIn.email,
+            followers: currentUserLoggedIn.followers,
+            following: followingTempArray,
+            fullName: currentUserLoggedIn.fullName,
+            posts: currentUserLoggedIn.posts,
+            profilePictureURL: currentUserLoggedIn.profilePictureURL,
+            username: currentUserLoggedIn.username,
+          })
+        );
+        console.log(
+          "Dispatched setUser action & Updated current user logged-in following array"
+        );
+      });
+
+    //update other user  followers array
+    const followersTempArray = [...otherUser.followers]; // Make a copy of the array
+    followersTempArray.push(currentUserLoggedIn.username);
+
+    await firebase
+      .firestore()
+      .collection("Users")
+      .doc(otherUser.username)
+      .update({
+        followers: followersTempArray,
+      })
+      .then(() => {
+        //update redux global state
+        dispatch(
+          OtherUserSlice.actions.setOtherUser({
+            description: otherUser.description,
+            email: otherUser.email,
+            followers: followersTempArray,
+            following: otherUser.following,
+            fullName: otherUser.fullName,
+            posts: otherUser.posts,
+            profilePictureURL: otherUser.profilePictureURL,
+            username: otherUser.username,
+          })
+        );
+        console.log("Updated other user followers array");
+      });
+
+    //setCurrentUserLoggedinFollowOtherUser
+    console.log(
+      "Follow functionality for " +
+        otherUser.username +
+        " executed with success!"
+    );
+    setCurrentUserLoggedinFollowOtherUser(true);
+  } else {
+    //update firestore and update redux global state
+    //update current user logged in following array
+    const followingTempArray = [...currentUserLoggedIn.following]; // Make a copy of the array
+
+    const userIndexFollowing = followingTempArray.indexOf(otherUser.username);
+
+    //delete the user that unliked the photo from the people that like array
+    followingTempArray.splice(userIndexFollowing, 1);
+
+    await firebase
+      .firestore()
+      .collection("Users")
+      .doc(currentUserLoggedIn.username)
+      .update({
+        following: followingTempArray,
+      })
+      .then(() => {
+        //update redux global state
+        dispatch(
+          UserSlice.actions.setUser({
+            description: currentUserLoggedIn.description,
+            email: currentUserLoggedIn.email,
+            followers: currentUserLoggedIn.followers,
+            following: followingTempArray,
+            fullName: currentUserLoggedIn.fullName,
+            posts: currentUserLoggedIn.posts,
+            profilePictureURL: currentUserLoggedIn.profilePictureURL,
+            username: currentUserLoggedIn.username,
+          })
+        );
+        console.log(
+          "Dispatched setUser action & Updated current user logged-in following array"
+        );
+      });
+
+    //update other user  followers array
+    const followersTempArray = [...otherUser.followers]; // Make a copy of the array
+
+    const userIndexFollowers = followersTempArray.indexOf(
+      currentUserLoggedIn.username
+    );
+
+    //delete the user that unliked the photo from the people that like array
+    followersTempArray.splice(userIndexFollowers, 1);
+
+    await firebase
+      .firestore()
+      .collection("Users")
+      .doc(otherUser.username)
+      .update({
+        followers: followersTempArray,
+      })
+      .then(() => {
+        //update redux global state
+        dispatch(
+          OtherUserSlice.actions.setOtherUser({
+            description: otherUser.description,
+            email: otherUser.email,
+            followers: followersTempArray,
+            following: otherUser.following,
+            fullName: otherUser.fullName,
+            posts: otherUser.posts,
+            profilePictureURL: otherUser.profilePictureURL,
+            username: otherUser.username,
+          })
+        );
+        console.log("Updated other user followers array");
+      });
+
+    //setCurrentUserLoggedinFollowOtherUser
+    console.log(
+      "Unfollow functionality for " +
+        otherUser.username +
+        " executed with success!"
+    );
+    setCurrentUserLoggedinFollowOtherUser(false);
+  }
+};
 
 const FollowOrUnfollowButton = () => {
   const [
     doesCurrentUserLoggedinFollowOtherUser,
     setCurrentUserLoggedinFollowOtherUser,
   ] = useState(false);
-
+  const dispatch = useDispatch();
   const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
   const currentUserLoggedIn = useSelector(
     (state) =>
@@ -34,7 +192,17 @@ const FollowOrUnfollowButton = () => {
   }, []);
 
   return (
-    <TouchableOpacity onPress={() => onPressHandler()}>
+    <TouchableOpacity
+      onPress={async () =>
+        onPressHandler(
+          doesCurrentUserLoggedinFollowOtherUser,
+          setCurrentUserLoggedinFollowOtherUser,
+          currentUserLoggedIn,
+          otherUser,
+          dispatch
+        )
+      }
+    >
       <View
         style={{
           backgroundColor: doesCurrentUserLoggedinFollowOtherUser
